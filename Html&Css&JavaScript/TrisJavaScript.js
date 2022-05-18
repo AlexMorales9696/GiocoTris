8d1cf44f5a8fcd7341c447db0361842943ba757e
AlexMorales9696 <103995519+AlexMorales9696@users.noreply.github.com>
2022-05-18 15:31:06 +0200

    Add files via upload
    iniziato il JavaScript fatta la parte in cui riempio la tabella in base al turno con x o o

--- Html&Css&JavaScript/TrisJavaScript.js
+++ Html&Css&JavaScript/TrisJavaScript.js
@@ -1,2 +1,17 @@
 const x=document.getElementsByClassName('stileBottone')
 console.log(x);
+turno=0;
+function myFunction(riga,colonna) {
+    n=riga*3+colonna;
+    if(turno%2==0){
+        document.getElementsByClassName('stileBottone')[n].innerHTML ="X";
+    }
+    else 
+    {
+        document.getElementsByClassName('stileBottone')[n].innerHTML ="O";
+    }
+   turno=turno+1;
+  }
+
+  turno=0;
+  
